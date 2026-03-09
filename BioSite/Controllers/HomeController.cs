adef934a86f76811d8b277c5decdf81bac98da93
elifyeter <elifyyeter@gmail.com>
2026-03-09 03:45:39 +0300

    Initial commit - Personal Portfolio Website

---
+++ BioSite/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using BioSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BioSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cv()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
